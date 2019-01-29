86e673642235436dc64607e369f3372510c4f294
emendezduran <emendezduran@nautilus25.danielcastelao.org>
2019-01-29 10:39:46 +0100

    commit final

--- empleado/Main.java
+++ empleado/Main.java
@@ -10,10 +10,15 @@ public class Main {
 
     public static void main(String[] args) {
 
-        GregorianCalendar fechaNac = new GregorianCalendar(1990, 10, 5);
-        GregorianCalendar fechaIni = new GregorianCalendar(2012, 10, 5);
-        Empleado secret1 = new Empleado("Maria", fechaNac, fechaIni, "Administracion Empresarial");
-        System.out.println(secret1.toString());
-        secret1.calcularSueldo();
+        //GregorianCalendar fechaNac = new GregorianCalendar(1990, 10, 5);
+        //GregorianCalendar fechaIni = new GregorianCalendar(2012, 10, 5);
+        
+        Empleado empleado1 = new Empleado("Maria", new GregorianCalendar(1990, 10, 5),new GregorianCalendar(2012, 10, 5), "Administracion Empresarial");
+        System.out.println(empleado1.toString());
+        empleado1.calcularSueldo();
+        
+        Secretaria secretaria1 = new Secretaria("660459006","Ana", new GregorianCalendar(1995, 8, 3),new GregorianCalendar(2014, 10, 2),"Secretariado");
+        System.out.println(secretaria1.getNombre());
+        secretaria1.calcularSueldo();
     }
 }
--- empleado/Secretaria.java
+++ empleado/Secretaria.java
@@ -5,7 +5,15 @@ import java.util.GregorianCalendar;
 public class Secretaria extends Empleado {
 
     private String telefonoMovil;
+    
+    public Secretaria() {
+    }
 
+    public Secretaria(String telefonoMovil) {
+       super(); 
+        this.telefonoMovil = telefonoMovil;
+    }
+    
     public Secretaria(String telefonoMovil, String nombre, GregorianCalendar inicioTrabajo, GregorianCalendar fechaNacimiento, String titulacion) {
         super(nombre, inicioTrabajo, fechaNacimiento, titulacion);
         this.telefonoMovil = telefonoMovil;
@@ -18,7 +26,12 @@ public class Secretaria extends Empleado {
     public void setTelefonoMovil(String telefonoMovil) {
         this.telefonoMovil = telefonoMovil;
     }
-
+    
+    @Override
+    public void calcularSueldo() {
+        System.out.println("Resultado del Calculo de Sueldo SECRETARIA");
+    }
+    
     @Override
     public String toString() {
         return "Secretaria{" + "telefonoMovil=" + telefonoMovil + '}';
